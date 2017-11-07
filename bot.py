4b1b7299663846eac9eff9b3bd8df71e13de0644
Sarah Cline <cline.s@husky.neu.edu>
2017-11-07 16:40:06 -0500

    Login Setup

--- bot.py
+++ bot.py
@@ -1,4 +1,23 @@
-# import praw
-# busterroni pip install praw
 # install pip
+
+# https://www.youtube.com/watch?v=krTUf7BpTc0
+
+import praw
+import config
+
+# busterroni$ pip install praw
+
+
+def bot_login():
+    login = praw.Reddit(username = config.username,
+        password = config.password,
+        client_id = config.client_id,
+        client_secret = config.client_secret,
+        user_agent = "test bot v0.1")
+    return login
+
+def run_bot(login):
+
+
+login = bot_login()
 
---
+++ config.py
@@ -0,0 +1,4 @@
+username = "bluestocking355"
+# password =
+# client_id =
+# client_secret = 
